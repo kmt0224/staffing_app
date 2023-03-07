7c74a6ee4e92c6693917c07a76861fa75291be3d
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-03-08 05:53:21 +0900

    indexアクションの実装

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,2 +1,25 @@
 class PositionsController < ApplicationController
+  def index
+    get_week
+  end
+
+  private
+  def get_week
+    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)','(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+    @todays_date = Date.today
+
+    @week_days = []
+
+    14.times do |x|      
+      wday_num = Date.today.wday + x
+      if wday_num >= 14
+        wday_num = wday_num - 14
+      end
+
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, wday: wdays[wday_num]}
+
+      @week_days.push(days)
+      end
+    end
+
 end
