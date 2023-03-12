a329378e7cad6b431f091b4549ee7ac14888dd41
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-03-12 23:48:23 +0900

    メンバー登録の実装

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,14 @@
 class MembersController < ApplicationController
+  def index
+    @members = Member.all
+  end
+
+  def create
+    @member = Member.create(member_params)
+  end
+
+  private
+  def member_params
+    params.permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "staffings#index"
   resources :staffings, only: [:index, :edit]
+  resources :members, only: [:index, :create]
 end
