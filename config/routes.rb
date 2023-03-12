36f7a3c488b5b4141265f1ad0f5fa3cad0fa70f6
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-03-13 06:41:56 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "staffings#index"
   resources :staffings, only: [:index, :edit]
   resources :members, only: [:index, :create]
+  resources :positions, only: [:index, :create]
 end
