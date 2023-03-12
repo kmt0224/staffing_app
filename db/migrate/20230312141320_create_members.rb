3ea88806eda5845ec3dd73a2944a44c957b76a8e
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-03-12 23:18:47 +0900

    マイグレーション作成

--- db/migrate/20230312141320_create_members.rb
+++ db/migrate/20230312141320_create_members.rb
@@ -1,7 +1,7 @@
 class CreateMembers < ActiveRecord::Migration[6.0]
   def change
     create_table :members do |t|
-
+      t.string :name, null: false
       t.timestamps
     end
   end
--- db/migrate/20230312141348_create_positions.rb
+++ db/migrate/20230312141348_create_positions.rb
@@ -1,7 +1,7 @@
 class CreatePositions < ActiveRecord::Migration[6.0]
   def change
     create_table :positions do |t|
-
+      t.string :position, null: false
       t.timestamps
     end
   end
