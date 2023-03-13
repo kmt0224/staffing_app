f85f36fb6e918c66c9a350e97ade1c1a7a2ef0d2
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-03-14 06:34:32 +0900

    配置箇所削除機能の実装

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -7,6 +7,12 @@ class PositionsController < ApplicationController
     @position = Position.create(position_params)
   end
 
+  def destroy
+    @position = Position.find(params[:id])
+    @osition.destroy
+    redirect_to root_path
+  end
+
   private
   def position_params
     params.permit(:position)
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root "staffings#index"
   resources :staffings, only: [:index, :edit]
   resources :members, only: [:index, :create, :destroy]
-  resources :positions, only: [:index, :create]
+  resources :positions, only: [:index, :create, :destroy]
 end
