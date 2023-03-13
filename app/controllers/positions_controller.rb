5731696c199780286396485364338fa9dc16d746
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-03-14 05:57:31 +0900

    配置箇所登録機能の実装

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,8 +1,14 @@
 class PositionsController < ApplicationController
   def index
-
+    @positions = Position.all
   end
 
   def create
+    @position = Position.create(position_params)
+  end
+
+  private
+  def position_params
+    params.permit(:position)
   end
 end
