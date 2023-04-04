b292b79c40dbcc5aee6b7bb2837c74f20ba5a372
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-04-04 15:08:22 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root "staffings#index"
-  resources :staffings, only: [:index, :edit]
+  resources :staffings, only: [:index, :create]
   resources :members, only: [:index, :create, :destroy]
   resources :positions, only: [:index, :create, :destroy]
+  resources :start_days, only: [:index, :create] do
+    collection do
+      delete 'destroy_all'
+    end
+  end
 end
