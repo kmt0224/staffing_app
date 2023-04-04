43ff0004d3195e5a5b27cd147cd74574a8952e34
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-04-04 15:08:44 +0900

    staffingsテーブル編集

--- db/migrate/20230312141918_create_staffings.rb
+++ db/migrate/20230312141918_create_staffings.rb
@@ -4,7 +4,6 @@ class CreateStaffings < ActiveRecord::Migration[6.0]
       t.references :member, null: false, foreign_key: true
       t.references :position, null: false, foreign_key: true
       t.date :date, null: false
-      t.date :startDate, null: false
       t.timestamps
     end
   end
