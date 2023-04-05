ed3f0cccf3b31562bd140fa785713b9d8a983113
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-04-05 21:24:53 +0900

    トップページ修正

--- app/controllers/staffings_controller.rb
+++ app/controllers/staffings_controller.rb
@@ -3,6 +3,9 @@ class StaffingsController < ApplicationController
 
   def index
     @get_week = get_week
+    @members = Member.all
+    @positions = Position.all.map {|position| [position.position, position.id]}
+    @staffing = Form::StaffingCollection.new
   end
 
   def new
@@ -13,16 +16,27 @@ class StaffingsController < ApplicationController
   end
 
   def create
+    @get_week = get_week
+    @members = Member.all
+    @positions = Position.all.map {|position| [position.position, position.id]}
     @staffing =  Form::StaffingCollection.new(staffing_params)
-    if @staffing.save
-      redirect_to action: :index
-    else
-      render :staffings_path
-    end
+   if @staffing.save
+    redirect_to root_path
+   else
+    render :new
+   end
   end
 
+  def edit
   end
-  
+
+  def update
+  end
+
+  def destroy_all
+    Staffing.destroy_all
+    redirect_to root_path
+  end
 
   private
   def move_to_start_days
@@ -43,23 +57,32 @@ class StaffingsController < ApplicationController
     end
 
     @week_days = []
-
     
     staffings = Staffing.where(date: @todays_date..@todays_date + 13)
 
-    14.times do |x|      
+    today_staffings = []
+    staffings.each do |s|
+      today_staffing = {member: s.member.name, position: s.position.position, date: s.date}
+      today_staffings.push(today_staffing)
+    end
+    
+    14.times do |x|  
+      staffing = []
+      today_staffings.count.times do |i|
+        if today_staffings[i][:date] == @todays_date + x 
+          staffing.push(today_staffings[i][:position])
+        end
+      end
+
+
       wday_num = @todays_date.wday + x
+
       if wday_num >= 14
         wday_num = wday_num - 14
       end
-
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, wday: wdays[wday_num]}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, staffing: staffing, wday: wdays[wday_num]}
 
       @week_days.push(days)
-      end
     end
-
-    # def staffing_params
-    #   params.require(:staffing).permit(:startDate, :date).marge(member_id: @member.id, position_id: @position.id)
-    # end
+  end
 end
