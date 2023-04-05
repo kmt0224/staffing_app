b7ba649dd7c455265d41afb661e7e315b0a19574
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-04-05 14:02:54 +0900

    staffing保存機能の実装

--- app/controllers/staffings_controller.rb
+++ app/controllers/staffings_controller.rb
@@ -5,15 +5,35 @@ class StaffingsController < ApplicationController
     @get_week = get_week
   end
 
+  def new
+    @get_week = get_week
+    @members = Member.all
+    @positions = Position.all.map {|position| [position.position, position.id]}
+    @staffing = Form::StaffingCollection.new
+  end
+
   def create
+    @staffing =  Form::StaffingCollection.new(staffing_params)
+    if @staffing.save
+      redirect_to action: :index
+    else
+      render :staffings_path
+    end
+  end
+
   end
   
+
   private
   def move_to_start_days
     start_day = StartDay.all
     if start_day.blank?
       redirect_to start_days_path
     end
+  end
+
+  def staffing_params
+    params.require(:form_staffing_collection).permit(staffings_attributes: [:member_id, :position_id, :date])
   end
 
   def get_week
