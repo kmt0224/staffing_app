430c7a8931b5f4ea26c59ccd5a979e5c2d2a9833
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-04-10 10:54:07 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
   end
   resources :members, only: [:index, :create, :destroy]
   resources :positions, only: [:index, :create, :destroy]
-  resources :start_days, only: [:index, :create] 
+  resources :start_days, only: [:index, :create] do
+    collection do
+      delete 'destroy_all'
+    end
+  end
 end
